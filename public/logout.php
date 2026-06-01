e2003513fe7b5bba5bfbb0091740023087bde9e4
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-06-01 09:32:37 -0300

    add - comentario logout

--- public/logout.php
+++ public/logout.php
@@ -4,3 +4,5 @@
     header("Location: ../index.php");
     exit();
 ?>
+
+<!--Esse código faz com que ao clicar em 'sair' na pagina 'home' os dados sejam "exlcuidos", fazendo com que seja necessário fazer o login novamente-->
