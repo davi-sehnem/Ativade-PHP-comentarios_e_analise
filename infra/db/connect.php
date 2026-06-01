2a2b6e5dfb3396404e19cfcaa99c6bc6f93f57b7
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-06-01 09:47:27 -0300

    add - comentário connect

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,16 +1,22 @@
 <?php
     session_start();
+    //Inicia ou continua uma sessão mo php
 
     $host = "localhost";
+    //localhost significa que o banco está na própria maquina
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
     
     $conn = new mysqli($host,$user,$pass,$db);
+    //conecta o banco de dados com as informações acima. Guarda da variavel '$conn'.
 
     // if($conn->connect_error){
     //     die("Erro na conexão");
     // }else{
     //     echo ("<p> BD: ok </p>");
     // }
+
+    //Se der errado mostra a mensagem 'erro na conexão'. Se der certo 'ok'.
 ?>
+
