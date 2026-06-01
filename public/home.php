86561f9ff5e4081a43e11d9f8f77c310a10a7740
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-06-01 11:08:05 -0300

    add - comentários home

--- public/home.php
+++ public/home.php
@@ -1,18 +1,21 @@
 <?php
-    include("../infra/db/connect.php");
+    include("../infra/db/connect.php"); //importa o arquivo connect
     if(!isset($_SESSION["usuario"])){
         header("Location: ../index.php");
-        exit();
+        exit(); //verifica se existe algum usuário logado, se não houver será mandado de volta para a pagina inicial e para o código 'exit'.
     }
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $usuario = $_POST["usuario"];
-        $senha = $_POST["senha"];
-        $sql = "INSERT INTO users (username, password) VALUES ('$usuario','$senha')";
-        if($conn -> query($sql) === TRUE){
-            echo "<script>alert('Usuário Cadastrado com sucesso!')</script>";
+        $senha = $_POST["senha"]; //Se a pagina for acessada pelo POST, os dados serão guardados na variavel 'usuario' e 'senha'.
+
+        $sql = "INSERT INTO users (username, password) VALUES ('$usuario','$senha')"; //insere as variaveis dentro da tabela
+
+        if($conn -> query($sql) === TRUE){ //envia o sql para o banco de dados com o: ($conn -> query($sql).
+            echo "<script>alert('Usuário Cadastrado com sucesso!')</script>"; //se der certo
         }else{
-            echo "<script>alert('Erro Usuário Não Cadastrado!')</script>";
-        }
+            echo "<script>alert('Erro Usuário Não Cadastrado!')</script>"; //se der errado
+        
+        } 
     }
 ?>
 
@@ -23,17 +26,17 @@
     <title>Home</title>
     <link rel="stylesheet" href="../style/style.css">
 </head>
-<body>
+<body> 
     <?php
-        include("../public/component/navbar.php");
+        include("../public/component/navbar.php"); //traz a navbar
     ?>
-    <h2>Bem-vindo!</h2>
+    <h2>Bem-vindo!</h2> 
     <p> Usuário logado: 
         <?php echo $_SESSION["usuario"];?>
     </p>
 
-    <h4>Cadastrar Novo Usuário</h4>
-    <form method="POST">
+    <h4>Cadastrar Novo Usuário</h4> <
+    <form method="POST"> <!-- o formulário envia os dados pelo metodo POST, que será processado codigo acima -->
         <label for="usuario">Usuario:</label>
         <input type="text" name="usuario">
         <br>
@@ -42,15 +45,16 @@
         <input type="password" name="senha">
         <br>
         <br>
-        <button type="submit">Cadastrar</button>
-    </form>
+        <button type="submit">Cadastrar</button> 
+    </form> 
     <?php
     
-    include("../public/component/table.php");
+    include("../public/component/table.php"); //traz a tabela
     ?>
 
 
     <a href="logout.php">Sair</a>
     
-</body>
+</body> 
 </html>
+
